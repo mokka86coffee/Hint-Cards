4ac0ebbb941cadf5e0f886b84698afd7fde776a6
mokka86coffee <mokka86coffee@gmail.com>
2019-05-18 18:59:57 +0300

    commit

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,35 +2,39 @@ import React from "react";
 import { inject, observer } from "mobx-react";
 import styles from "./Card.scss";
 
-class Card extends React.Component {
-  render() {
-    const { users } = this.props.UsersStore;
+export default inject("UsersStore")(observer(Card));
 
-    if (!users.length) {
-      return null;
-    }
+function Card({ UsersStore }) {
+  const { users } = UsersStore;
 
-    return users.map(({ username, email, name, id }) => (
-      <div className={styles.card} key={id}>
-        <p>{name}</p>
-        <span>{username}</span>
-        <h4>{email}</h4>
-      </div>
-    ));
+  if (!users.length) {
+    return null;
   }
+
+  return users.map(({ username, email, name, id }) => (
+    <div className={styles.cards} key={id}>
+      <h2>{name}</h2>
+      <span>{username}</span>
+      <code>{email}</code>
+    </div>
+  ));
 }
-export default inject("UsersStore", "BirdsStore")(observer(Card));
 
-// function Card(props) {
-//   console.log(props);
-//   const { username, company, email, id } = props.UsersStore.getUserInfo;
-//   return (
-//     <div className={styles.card} key={id}>
-//       <h2>{username}</h2>
-//       <h3>{email}</h3>
-//       <h4>{company}</h4>
-//     </div>
-//   );
-// }
-
-// export default inject("UsersStore")(observer(Card));
+{
+  // class Card extends React.Component {
+  //   render() {
+  //     const { users } = this.props.UsersStore;
+  //     if (!users.length) {
+  //       return null;
+  //     }
+  //     return users.map(({ username, email, name, id }) => (
+  //       <div className={styles.card} key={id}>
+  //         <p className={styles.t}>{name}</p>
+  //         <span className={styles.c}>{username}</span>
+  //         <span className={styles.i}>{email}</span>
+  //       </div>
+  //     ));
+  //   }
+  // }
+  // export default inject("UsersStore", "BirdsStore")(observer(Card));
+} // Class version
