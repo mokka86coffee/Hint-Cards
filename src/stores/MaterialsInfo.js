bd909485855ac34a5df523f9b0be95089ce82242
Evgeniy_Marushev@epam.com <Evgeniy_Marushev@epam.com>
2019-05-23 00:44:01 +0300

    added observer info

--- src/stores/MaterialsInfo.js
+++ src/stores/MaterialsInfo.js
@@ -205,13 +205,44 @@ var Materials = {
       link: "https://lodash.com/docs/4.17.11#zip"
     }
   ],
-  zzzzzz: [
+  Javascript: [
     {
-      title: `zzzzzzzzzzz`,
-      text: `zzzzzzzzz`,
+      title: `Observer + Observable`,
+      text: `<c>
+      'use strict';
+
+      const randomChar = () => String
+        .fromCharCode(Math.floor((Math.random() * 25) + 97));
+      
+      const subscribe = observer => {
+        const observable = { observer };
+        setInterval(() => {
+          const char = randomChar();
+          observer(char);
+        }, 200);
+        return observable;
+      };
+      
+      // Usage
+      
+      let count = 0;
+      
+      const observer = char => {
+        process.stdout.write(char);
+        count++;
+        if (count > 50) {
+          process.stdout.write('\n');
+          process.exit(0);
+        }
+      };
+      
+      const observable = subscribe(observer);
+      
+      console.dir({ observer, observable });
+      </c>`,
       id: uuidv4(),
       tags: "",
-      link: "zzzzzzzzzz"
+      link: "https://www.youtube.com/watch?v=_bFXuLcXoXg"
     }
   ],
   zzzzzz2: [
